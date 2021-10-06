8b8d7d1a3a3dfa9b0806acceeda93f155e989535
Amanda Kays <amandakays89@gmail.com>
2021-10-06 10:10:50 -0500

    seeded user data and able to add user through graphql server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/familyvaca', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/travel-app', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
