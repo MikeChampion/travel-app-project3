416aede3e5f6c40fab9ab620ad39b6fc83556a19
Amanda Kays <amandakays89@gmail.com>
2021-10-07 16:51:19 -0500

    Travel schema

---
+++ server/models/Travel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const travelSchema = new Schema  ({
+    who: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    how: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    arrive: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    depart: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    
+     postedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+      }
+});
+
+    const Travel = mongoose.model('Travel', travelSchema);
+
+module.exports = Travel;
