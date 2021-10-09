13843e9af0972c7b907cc00774c0497ef25f52c9
Amanda Kays <amandakays89@gmail.com>
2021-10-09 15:06:57 -0500

    add avtivity and add travel mutation for appollo client

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -31,3 +31,53 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const ADD_ACTIVITY = gql `
+mutation addActivity (
+  $where: String!
+  $when: String!
+  $description: String!
+  $postedBy: User
+){
+  addActivity(
+    where: $where
+    when: $when
+    description: $description
+    postedBy: $postedBy
+
+  ) {
+    _id
+    where 
+    when
+    description
+    postedBy
+  }
+}
+`
+
+export const ADD_TRAVEL = gql `
+mutation addActivity (
+  $who: String!
+  $how: String!
+  $arrive: String!
+  $depart: String!
+  $postedBy: User
+){
+  addActivity(
+    who: $who
+    how: $how
+    arrive: $arrive
+    depart: $depart
+    postedBy: $postedBy
+
+  ) {
+    _id
+    who
+    how
+    arrive
+    depart
+    postedBy
+  }
+}
+`
+
