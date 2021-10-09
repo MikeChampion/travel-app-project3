ef79481ddeaf9bc85ed07f9207e88a945a8625f6
Amanda Kays <amandakays89@gmail.com>
2021-10-09 15:18:38 -0500

    user query for apollo client

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -5,18 +5,9 @@ export const QUERY_USER = gql`
     user {
       firstName
       lastName
-      orders {
-        _id
-        purchaseDate
-        products {
-          _id
-          name
-          description
-          price
-          quantity
-          image
+      email
         }
       }
-    }
-  }
-`;
+    `;
+
+    
