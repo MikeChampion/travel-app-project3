e469a194ab2e1ce718f749dfd3063e6923ccb835
Jason Viox <jrv04@hotmail.com>
2021-10-10 20:53:50 -0500

    Add event request to service-worker

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -18,7 +18,39 @@ const FILES_TO_CACHE = [
             caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
         );
     
-        // self.skipWaiting();
     });
 
     
+    self.addEventListener("fetch", function(evt) {
+        if (evt.request.url.includes("/api")) {
+            evt.respondWith(
+                caches.open(DATA_CACHE_NAME).then(cache => {
+                    return fetch(evt.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+    
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(evt.request);
+                    });
+                }).catch(err => console.log(err))
+            );
+    
+            return;
+        }
+        evt.respondWith(
+            fetch(evt.request).cache(() => {
+                return caches.match(evt.request).then(response => {
+                    if (response) {
+                        return response
+                    }
+                    if (evt.request.headers.get("accept").includes("text/html")){
+                        return caches.match("/")
+                    }
+                })
+            })
+        );
+    });
