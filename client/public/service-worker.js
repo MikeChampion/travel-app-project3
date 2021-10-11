2a6a06227a860afc2bbb84921ee90da1b7bfb340
Jason Viox <jrv04@hotmail.com>
2021-10-10 20:43:59 -0500

    Add files to cache

---
+++ client/public/service-worker.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/logo192.png",
+    "/logo512.png",
+    "/manifest.json",
+    "tailwind.config.js",
+    "/manifest.webmanifest",
+    "favicon.ico"
+    ];
+
+    
