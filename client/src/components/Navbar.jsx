b7279596ed765f33ee43cb5499b04084924cc44c
Zathras <ChampionMike42@gmail.com>
2021-10-12 21:19:13 -0500

    Remove unused import

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import UserContext from "../context/UserContext";
 import PropTypes from "prop-types";
 import React from "react";
 import { NavLink } from "react-router-dom";
-import authService from "services/authService";
+
 
 const navLinks = [
   {
@@ -17,7 +17,6 @@ const navLinks = [
 
 function Navbar(props) {
     const [user] = React.useContext(UserContext);
-
     return (
         <nav className="flex flex-row justify-between items-center w-full px-4 py-2 bg-yellow-700 text-white">
             <div className="flex">
@@ -31,36 +30,26 @@ function Navbar(props) {
                     </NavLink>
                 ))}
             </div>
-            {/* <div className=""> 
-                <NavLink to="/logout" activeClassName='bg-yellow-300 text-green-800 hover:text-yellow-900' className="text-sm md:text-lg px-2 py-1 underline rounded-lg hover:text-yellow-300 transition">Logout</NavLink>
-                    
-                <NavLink to="/login" activeClassName='bg-yellow-300 text-green-800 hover:text-yellow-900' className="text-sm md:text-lg px-2 py-1 underline rounded-lg hover:text-yellow-300 transition">Login</NavLink>
-            </div> */}
 
             <div>
-                {/* Check the token for a valid login. If there is one, who is it? */}
-                {authService.checkIfLoggedIn() && user ? (
-                /**
-                * TODO: onClick:
-                * 1. authService.logout()
-                * 2. setUser(null)
-                */
-                <NavLink
+               {user?.data ?  <NavLink
                     to="/logout"
                     activeClassName="bg-yellow-300 text-green-800 hover:text-yellow-900"
                     className="text-sm md:text-lg px-2 py-1 underline rounded-lg hover:text-yellow-300 transition"
                 >
                     Logout
-                </NavLink>
-                ) : (
-                <NavLink
+                </NavLink> :  <NavLink
                     to="/login"
                     activeClassName="bg-yellow-300 text-green-800 hover:text-yellow-900"
                     className="text-sm md:text-lg px-2 py-1 underline rounded-lg hover:text-yellow-300 transition"
                 >
                     Login
-                </NavLink>
-                )}
+                </NavLink> }
+                 
+               
+               
+                
+                
             </div>
             
         </nav>
