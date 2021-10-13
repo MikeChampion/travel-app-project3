926fb5c72678ba6550fa8d40b2bd8fcfc4f8173d
Zathras <ChampionMike42@gmail.com>
2021-10-12 20:56:45 -0500

    Add a return to render components

--- client/src/components/AddUser.jsx
+++ client/src/components/AddUser.jsx
@@ -1,41 +1,11 @@
-// import React, { useState } from 'react';
-// import { useMutation } from '@apollo/client';
-// import Auth from '../utils/auth';
-// import { ADD_USER } from '../utils/mutations';
-// import AdminContext from '../context/AdminContext';
-
 function Signup() {
-    // const handleSignupSubmit = async (event) => {
-    //     event.preventDefault();
-    //     const newSignup = Object.fromEntries(new FormData(event.target))
-    
-    //     const mutationResponse = await addUser({
-    //         variables: newSignup
-    //     });
-    //     const token = mutationResponse.data.addUser.token;
-    //     Auth.login(token);
-    //   };
-
-    // const [formState, setFormState] = useState({ email: '', password: '' });
-
-    // const [addUser] = useMutation(ADD_USER, {
-    //     onCompleted() {
-    //         console.log("Update admin context with logged in status")
-    //         setAdmin(true)
-    //     }
-    // });
-
-    <div id="signup" className="flex flex-col justify-start items-center p-4 w-max border-yellow-600 border-2 bg-yellow-300 rounded-lg">
+return(  <div id="signup" className="flex flex-col justify-start items-center p-4 w-max border-yellow-600 border-2 bg-yellow-300 rounded-lg">
         <h2 className="font-bold">Signup</h2>
         <form className="signup-form flex flex-col justify-center items-center gap-2">
             {/*  onChange={handleChange}  */}
             <div className="flex flex-col justify-center items-center">
-                <label htmlFor="firstName" className="font-bold self-start">first name:</label>
-                <input className="form-input" type="text" id="firstName" name="firstName" />
-            </div>
-            <div className="flex flex-col justify-center items-center">
-                <label htmlFor="lastName" className="font-bold self-start">last name:</label>
-                <input className="form-input" type="text" id="lastName" name="lastName" />
+                <label htmlFor="userName" className="font-bold self-start">first name:</label>
+                <input className="form-input" type="text" id="userName" name="userName" />
             </div>
             <div className="flex flex-col justify-center items-center">
                 <label htmlFor="email" className="font-bold self-start">email:</label>
@@ -54,6 +24,7 @@ function Signup() {
             >signup</button>
         </form>
     </div>
+)
 }
 
 export default Signup
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,36 +1,7 @@
-// import React, { useState } from 'react';
-// import { useMutation } from '@apollo/client';
-// import Auth from '../utils/auth';
-// import { ADD_USER } from '../utils/mutations';
-// import AdminContext from '../context/AdminContext';
-
-function Login() {
-    // const handleLoginSubmit = async (event) => {
-    //     event.preventDefault();
-    //     const loginSubmission = Object.fromEntries(new FormData(event.target))
-
-    //     try {
-    //       const mutationResponse = await login({
-    //         variables: loginSubmission,
-    //       });
-    //       const token = mutationResponse.data.login.token;
-    //       Auth.login(token);
-    //     } catch (e) {
-    //       console.log(e);
-    //     }
-    //   };
-    
-    //   const handleChange = (event) => {
-    //     const { name, value } = event.target;
-    //     setFormState({
-    //       ...formState,
-    //       [name]: value,
-    //     });
-    //   };
-
-    <div id="login" className="flex flex-col justify-start items-center p-4 w-max border-yellow-600 border-2 bg-yellow-300 rounded-lg">
+function Login(props) {
+    return(    <div id="login" className="flex flex-col justify-start items-center p-4 w-max border-yellow-600 border-2 bg-yellow-300 rounded-lg">
         <h2 className="font-bold">Login</h2>
-        <form className="login-form flex flex-col justify-center items-center gap-2">
+        <form onSubmit={props.submitHandler} className="login-form flex flex-col justify-center items-center gap-2">
             <div className="flex flex-col justify-center items-center">
                 <label htmlFor="email" className="font-bold self-start">email:</label>
                 <input className="form-input" type="text" id="email" name="email" />
@@ -48,6 +19,7 @@ function Login() {
             >login</button>
         </form>
     </div>
+)
 }
 
 export default Login
