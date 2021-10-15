99b56747858a4f168d2caf98cbaec0bfe31e97e6
Zathras <ChampionMike42@gmail.com>
2021-10-14 23:01:02 -0500

    Correct uri variable

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import { setContext } from '@apollo/client/link/context';
 Modal.setAppElement('#root')
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:3001/graphql',
+    uri: '/graphql',
   });
 
   const authLink = setContext((_, { headers }) => {
